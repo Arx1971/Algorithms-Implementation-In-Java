789a9dccdadd604b6abfe0b480ca861021090d81
Arx1971 <adnan.rahin13@gmail.com>
2020-04-02 13:46:04 -0400

    Depth First Search

---
+++ src/GraphTraversal/BreadthFirstSearch/Main.java
@@ -0,0 +1,4 @@
+package GraphTraversal.BreadthFirstSearch;
+
+public class Main {
+}
---
+++ src/GraphTraversal/DepthFirstSearch/Main.java
@@ -0,0 +1,47 @@
+package GraphTraversal.DepthFirstSearch;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        GraphTraversal(new int[][]{{0, 2}, {2, 3}, {1, 2}, {3, 4}}, 5);
+
+    }
+
+    public static void GraphTraversal(int edges[][], int node) {
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for (int i = 0; i < node; i++)
+            graph.put(i, new ArrayList<>());
+
+        for (int i = 0; i < edges.length; i++) {
+            graph.get(edges[i][0]).add(edges[i][1]);
+            graph.get(edges[i][1]).add(edges[i][0]);
+        }
+
+        boolean visited[] = new boolean[node];
+
+        depthFirstSearch(graph, 0, visited, new Stack<>());
+    }
+
+    public static void depthFirstSearch(Map<Integer, List<Integer>> graph, int node, boolean[] visited, Stack<Integer> stack) {
+
+        visited[node] = true;
+
+        System.out.print(node + " ");
+
+        List<Integer> nodes = graph.get(node);
+
+        for (int i = 0; i < nodes.size(); i++) {
+            if (!visited[nodes.get(i)]) {
+                visited[nodes.get(i)] = true;
+                depthFirstSearch(graph, nodes.get(i), visited, stack);
+            }
+        }
+        stack.push(node);
+    }
+
+}
