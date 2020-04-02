6995d4c98687332e39839570c523a398700b5926
Arx1971 <adnan.rahin13@gmail.com>
2020-04-02 04:14:24 -0400

    Sliding Window SubArray Sum Equals To Target

---
+++ src/SlidingWindow/SubArraySum/Main.java
@@ -0,0 +1,33 @@
+package SlidingWindow.SubArraySum;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        System.out.println(Arrays.toString(subArraySumEqualsToTarget(new int[]{2, 5}, 8)));
+
+    }
+
+    public static int[] subArraySumEqualsToTarget(int nums[], int target) {
+
+        int left = 0;
+        int right = 1;
+        int sum = nums[left];
+
+        while (right < nums.length) {
+            if (sum < target)
+                sum += nums[right++];
+            else sum -= nums[left++];
+
+            if (sum == target)
+                return Arrays.copyOfRange(nums, left, right);
+        }
+
+        return new int[]{-1, -1};
+
+    }
+
+}
