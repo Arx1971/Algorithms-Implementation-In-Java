b458ae4b99fef810cbdf55aaff4c29d92350af48
Arx1971 <adnan.rahin13@gmail.com>
2020-04-03 07:28:20 -0400

    Topological Sorting

--- src/Graph/TopologicalSorting/Main.java
+++ src/Graph/TopologicalSorting/Main.java
@@ -5,11 +5,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String args[]) {
-
-        TopologicalSorting(new int[][]{{0, 1}, {1, 4}, {2, 3}, {3, 4}}, 5);
-        TopologicalSorting(new int[][]{{0, 1}, {1, 4}, {4, 3}, {3, 1}}, 5);
-        TopologicalSorting(new int[][]{{0, 1}, {1, 2}, {2, 0}}, 3);
-
+        TopologicalSorting(new int[][]{{5, 0}, {5, 2}, {2, 3}, {4, 0}, {1, 3}}, 6);
     }
 
 
@@ -31,6 +27,28 @@ public class Main {
             return;
         }
 
+        Arrays.fill(visited, false);
+        Stack<Integer> sortedList = new Stack<>();
+        for (int i = 0; i < nodes; i++)
+            if (!visited[i])
+                topologicalSort(graph, i, visited, sortedList);
+
+        System.out.println(sortedList);
+
+    }
+
+    public static void topologicalSort(Map<Integer, List<Integer>> graph, int src, boolean[] visited, Stack<Integer> stack) {
+
+        visited[src] = true;
+
+        List<Integer> edges = graph.get(src);
+
+        for (int i = 0; i < edges.size(); i++) {
+            if (!visited[edges.get(i)]) {
+                topologicalSort(graph, edges.get(i), visited, stack);
+            }
+        }
+        stack.add(src);
     }
 
     public static boolean GraphHasCycle(Map<Integer, List<Integer>> graph, int src, Set<Integer> set, boolean[] visited) {
@@ -39,12 +57,12 @@ public class Main {
 
         set.add(src);
 
-        List<Integer> nodes = graph.get(src);
+        List<Integer> edges = graph.get(src);
 
-        for (int i = 0; i < nodes.size(); i++) {
-            if (!visited[nodes.get(i)]) {
-                if (GraphHasCycle(graph, nodes.get(i), set, visited)) return true;
-            } else if (set.contains(nodes.get(i))) return true;
+        for (int i = 0; i < edges.size(); i++) {
+            if (!visited[edges.get(i)]) {
+                if (GraphHasCycle(graph, edges.get(i), set, visited)) return true;
+            } else if (set.contains(edges.get(i))) return true;
         }
         set.remove(src);
         return false;
