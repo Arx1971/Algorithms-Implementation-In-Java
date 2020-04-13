e1351d4699c0b8d07510c6d01c690860084e341d
Arx1971 <adnan.rahin13@gmail.com>
2020-04-12 20:57:21 -0400

    Path Compression Added

--- src/Graph/MinimumSpanningTreeKruskal/Main.java
+++ src/Graph/MinimumSpanningTreeKruskal/Main.java
@@ -1,42 +1,48 @@
 package Graph.MinimumSpanningTreeKruskal;
 
-import java.util.HashSet;
+
+import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        minimumSpanningTree(new int[][]{{0, 1, 1}, {1, 3, 5}, {0, 3, 6}, {0, 2, 9}, {2, 3, 8}});
+        minimumSpanningTree(new int[][]{{0, 1, 1}, {1, 3, 5}, {0, 3, 6}, {0, 2, 9}, {2, 3, 8}}, 4);
         minimumSpanningTree(new int[][]{{0, 1, 6}, {0, 2, 3}, {0, 4, 7}, {1, 3, 9}, {1, 4, 5}, {1, 2, 4},
-                {2, 4, 2}, {3, 4, 8}, {3, 5, 8}, {4, 5, 1}});
+                {2, 4, 2}, {3, 4, 8}, {3, 5, 8}, {4, 5, 1}}, 6);
+        minimumSpanningTree(new int[][]{{0, 1, 1}, {1, 3, 5}, {2, 3, 1}, {0, 2, 1}, {2, 1, 1}}, 4);
 
-        minimumSpanningTree(new int[][]{{0, 1, 1}, {1, 3, 5}, {2, 3, 1}, {0, 2, 1}, {2, 1, 1}});
+        minimumSpanningTree(new int[][]{{0, 1, 4}, {0, 7, 8}, {1, 2, 8}, {1, 7, 11}, {2, 3, 7}, {2, 8, 2},
+                {2, 5, 4}, {3, 4, 9}, {3, 5, 14}, {4, 5, 10}, {5, 6, 2}, {6, 7, 1}, {6, 8, 6}, {7, 8, 7}}, 9);
 
     }
 
-    public static void minimumSpanningTree(int[][] graph) {
+    public static void minimumSpanningTree(int[][] graph, int nodes) {
 
         PriorityQueue<Edge> priorityQueue = new PriorityQueue<>();
-
-        Set<Integer> unionFind = new HashSet<>();
 
         int minimumDistance = 0;
 
         for (int i = 0; i < graph.length; i++)
             priorityQueue.add(new Edge(graph[i][0], graph[i][1], graph[i][2]));
 
+        int parent[] = new int[nodes];
+
+        for (int i = 0; i < nodes; i++)
+            parent[i] = i;
+
+
         while (!priorityQueue.isEmpty()) {
 
             Edge edge = priorityQueue.poll();
 
-            if (unionFind.contains(edge.src) && unionFind.contains(edge.dst)) {
-                continue;
-            } else {
+            int x = find(parent, edge.src);     // find the parent of source
+            int y = find(parent, edge.dst);     // find the parent of destination
+
+            if (x != y) {                       // if x==y source and destination are in same set is cycle exist
                 minimumDistance += edge.cost;
-                unionFind.add(edge.src);
-                unionFind.add(edge.dst);
+                union(parent, x, y);
                 System.out.println(edge);
             }
 
@@ -46,6 +52,29 @@ public class Main {
 
     }
 
+    /**
+     * path compression
+     **/
+
+    public static int find(int[] parent, int vertex) {
+        if (parent[vertex] != vertex)
+            return find(parent, parent[vertex]);
+        return vertex;
+    }
+
+    public static void union(int[] parent, int x, int y) {
+        int x_set_parent = find(parent, x);
+        int y_set_parent = find(parent, y);
+        parent[y_set_parent] = x_set_parent;
+    }
+
+    /**
+     * initial set ->  {a}     {b}     {c}     {d}       {e}
+     * path {a ,b}     {a,b}           {c}     {d}       {e}
+     * path {c, e}     {a,b}           {c, e}  {d}
+     * path {a, c}     {a,b,c,e}               {d}
+     * path {c,d}      {a,b,c,e,d}
+     **/
 
     public static class Edge implements Comparable<Edge> {
         int src;
