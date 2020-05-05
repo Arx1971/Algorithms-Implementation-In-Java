b3ef763d9437063363dc523ffe4c05f49d855306
Arx1971 <adnan.rahin13@gmail.com>
2020-05-05 02:07:46 -0400

    Bitwise And

--- src/BitManipulation/BasicsOperators/Main.java
+++ src/BitManipulation/BasicsOperators/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String args[]) {
         System.out.println(bitwiseNot(5));
         System.out.println(bitwiseOr(2, 3));
+        System.out.println(bitwiseAnd(2, 3));
     }
 
     public static int bitwiseNot(int number) {
@@ -22,4 +23,14 @@ public class Main {
 
         return a | b;
     }
+
+    public static int bitwiseAnd(int a, int b) {
+        /**
+         *  a = 2 = 0010
+         *  b = 3 = 0011
+         *  a & b = 0010 = 2
+         * **/
+        return a & b;
+    }
+
 }
