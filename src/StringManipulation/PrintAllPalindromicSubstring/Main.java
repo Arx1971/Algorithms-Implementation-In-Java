15f30afc2655ee30c0160f9fffde2fd6070eb3e3
Arx1971 <adnan.rahin13@gmail.com>
2020-08-29 13:04:30 -0400

    TLE

--- src/StringManipulation/PrintAllPalindromicSubstring/Main.java
+++ src/StringManipulation/PrintAllPalindromicSubstring/Main.java
@@ -7,29 +7,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(printAllPalindromicSubstring("abcbc"));
+        System.out.println(BruteForce.printAllPalindromicSubstring("abcbc"));
 
     }
 
 
-    public static List<String> printAllPalindromicSubstring(String str) {
-        List<String> allPalindromicSubstring = new ArrayList<>();
+    public static class BruteForce {
+        public static List<String> printAllPalindromicSubstring(String str) {
+            List<String> allPalindromicSubstring = new ArrayList<>();
 
-        for (int i = 0; i < str.length(); i++) {
-            getLength(str, i, i, allPalindromicSubstring);
-            getLength(str, i, i + 1, allPalindromicSubstring);
+            for (int i = 0; i < str.length(); i++) {
+                getLength(str, i, i, allPalindromicSubstring);
+                getLength(str, i, i + 1, allPalindromicSubstring);
+            }
+            return allPalindromicSubstring;
         }
-        return allPalindromicSubstring;
-    }
 
-    public static List<String> getLength(String str, int i, int j, List<String> palindromes) {
+        public static List<String> getLength(String str, int i, int j, List<String> palindromes) {
 
-        while (i >= 0 && j < str.length() && str.charAt(i) == str.charAt(j)) {
-            palindromes.add(str.substring(i, j + 1));
-            i--;
-            j++;
+            while (i >= 0 && j < str.length() && str.charAt(i) == str.charAt(j)) {
+                palindromes.add(str.substring(i, j + 1));
+                i--;
+                j++;
+            }
+            return palindromes;
         }
-        return palindromes;
     }
 
 }
