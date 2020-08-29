1935a10e19641a2647568dc723d2ec41accadd35
Arx1971 <adnan.rahin13@gmail.com>
2020-08-29 14:31:08 -0400

    Large Test Cased passed

--- src/StringManipulation/CountNumberOfPalindromicSubstringInAString/Main.java
+++ src/StringManipulation/CountNumberOfPalindromicSubstringInAString/Main.java
@@ -4,8 +4,80 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println(numberOfPalindromicSubstringInAString("fjdsklfjslkdjflksdfjlsdieaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanvineinvoienovnwoinvoiwenfefoefnoeinvonoinefdfs" +
+                "jflkdsjfsjfoenvneoivneoivneoinvowinvwneovnavnfnanvvjkejvnjsdnkdsjfniuwdfnkjsfnfjdsklfjslkdjflk" +
+                "sdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanvineinvoienovnwoinvoiwenfefoefnoeinvo" +
+                "noinefdfsjflkdsjfsjfoenvneoivneoivneoinvowinvwneovnavnfnanvvjkejvnjsdnkdsjfniuwdfnkjsfnfjdsklf" +
+                "jslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanvineinvoienovnwoinvoiwenfe" +
+                "foefnoeinvonoinefdfsjflkdsjfsjfoenvneoivneoivneoinvowinvwneovnavnfnanvvjkejvnjsdnkdsjfniuwdfn" +
+                "kjsfnfjdsklfjslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanvineinvoienov" +
+                "nwoinvoiwenfefoefnoeinvonoinefdfsjflkdsjfsjfoenvneoivneoivneoinvowinvwneovnavnfnanvvjkejvnjsd" +
+                "nkdsjfniuwdfnkjsfnfjdsklfjslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaan" +
+                "vineinvoienovnwoinvoiwenfefoefnoeinvonoinefdfsjflkdsjfsjfoenvneoivneoivneoinvowinvwneovnavnf" +
+                "nanvvjkejvnjsdnkdsjfniuwdfnkjsfnfjdsklfjslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaanvineinvoienovnwoinvoiwenfefoefnoeinvonoinefdfsjflkdsjfsjfoenvneoivneoivneoinv" +
+                "owinvwneovnavnfnanvvjkejvnjsdnkdsjfniuwdfnkjsfnfjdsklfjslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaanvineinvoienovnwoinvoiwenfefoefnoeinvonoinefdfsjflkdsjfsjfoenvneoivneoivneo" +
+                "invowinvwneovnavnfnanvvjkejvnjsdnkdsjfniuwdfnkjsfnfjdsklfjslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaanvineinvoienovnwoinvoiwenfefoefnoeinvonoinefdfsjflkdsjfsjfoenvneoivneoivneoinvowinvwneo" +
+                "vnavnfnanvvjkejvnjsdnkdsjfniuwdfnkjsfnfjdsklfjslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanvineinvoi" +
+                "enovnwoinvoiwenfefoefnoeinvonoinefdfsjflkdsjfsjfoenvneoivneoivneoinvowinvwneovnavnfnanvvjkejvnjsdnkdsjfniuw" +
+                "dfnkjsfnfjdsklfjslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanvineinvoienovnwoinvoiwenfefoefnoeinv" +
+                "onoinefdfsjflkdsjfsjfoenvneoivneoivneoinvowinvwneovnavnfnanvvjkejvnjsdnkdsjfniuwdfnkjsfnfjdsklfjslkdjflksd" +
+                "fjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanvineinvoienovnwoinvoiwenfefoefnoeinvonoinefdfsjflkdsjfsjfoenvne" +
+                "oivneoivneoinvowinvwneovnavnfnanvvjkejvnjsdnkdsjfniuwdfnkjsfnfjdsklfjslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaanvineinvoienovnwoinvoiwenfefoefnoeinvonoinefdfsjflkdsjfsjfoenvneoivneoivneoinvowinvwneovna" +
+                "vnfnanvvjkejvnjsdnkdsjfniuwdfnkjsfnfjdsklfjslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanvineinvoi" +
+                "enovnwoinvoiwenfefoefnoeinvonoinefdfsjflkdsjfsjfoenvneoivneoivneoinvowinvwneovnavnfnanvvjkejvnjsdnkdsjfniuw" +
+                "dfnkjsfnfjdsklfjslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanvineinvoienovnwoinvoiwenfefoefnoe" +
+                "invonoinefdfsjflkdsjfsjfoenvneoivneoivneoinvowinvwneovnavnfnanvvjkejvnjsdnkdsjfniuwdfnkjsfnfjdsklfjslkdjf" +
+                "lksdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanvineinvoienovnwoinvoiwenfefoefnoeinvonoinefdfsjflkdsjfs" +
+                "jfoenvneoivneoivneoinvowinvwneovnavnfnanvvjkejvnjsdnkdsjfniuwdfnkjsfnfjdsklfjslkdjflksdfjlsdieaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaanvineinvoienovnwoinvoiwenfefoefnoeinvonoinefdfsjflkdsjfsjfoenvneoivneoivneoinvo" +
+                "winvwneovnavnfnanvvjkejvnjsdnkdsjfniuwdfnkjsfnfjdsklfjslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaanvineinvoienovnwoinvoiwenfefoefnoeinvonoinefdfsjflkdsjfsjfoenvneoivneoivneoinvowinvwneovnavnfnanvvjkej" +
+                "vnjsdnkdsjfniuwdfnkjsfnfjdsklfjslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanvineinvoienovnwoinv" +
+                "oiwenfefoefnoeinvonoinefdfsjflkdsjfsjfoenvneoivneoivneoinvowinvwneovnavnfnanvvjkejvnjsdnkdsjfniuwdfnkjsfn" +
+                "fjdsklfjslkdjflksdfjlsdieaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanvineinvoienovnwoinvoiwenfefoefnoeinvonoine"));
     }
 
+    public static int numberOfPalindromicSubstringInAString(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            count += countPalindrome(str, i, i);
+            count += countPalindrome(str, i, i + 1);
+        }
+        return count;
+    }
 
+    public static int countPalindrome(String str, int i, int j) {
+        int counter = 0;
+        while (i >= 0 && j < str.length() && str.charAt(j) == str.charAt(i)) {
+            counter++;
+            i--;
+            j++;
+        }
+        return counter;
+    }
 
 }
