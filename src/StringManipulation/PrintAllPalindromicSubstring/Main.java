98369517f03836b9755040512af08e3d6cf590cb
Arx1971 <adnan.rahin13@gmail.com>
2020-08-29 12:57:40 -0400

    Generate All plindrome using BruteForce

--- src/StringManipulation/PrintAllPalindromicSubstring/Main.java
+++ src/StringManipulation/PrintAllPalindromicSubstring/Main.java
@@ -1,11 +1,35 @@
 package StringManipulation.PrintAllPalindromicSubstring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println(printAllPalindromicSubstring("abcbc"));
+
     }
 
 
+    public static List<String> printAllPalindromicSubstring(String str) {
+        List<String> allPalindromicSubstring = new ArrayList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            getLength(str, i, i, allPalindromicSubstring);
+            getLength(str, i, i + 1, allPalindromicSubstring);
+        }
+        return allPalindromicSubstring;
+    }
+
+    public static List<String> getLength(String str, int i, int j, List<String> palindromes) {
+
+        while (i >= 0 && j < str.length() && str.charAt(i) == str.charAt(j)) {
+            palindromes.add(str.substring(i, j + 1));
+            i--;
+            j++;
+        }
+        return palindromes;
+    }
 
 }
