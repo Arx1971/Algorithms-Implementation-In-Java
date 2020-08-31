02a0be6841272256a7c2753b7cd82e201ec9c7fc
Arx1971 <adnan.rahin13@gmail.com>
2020-08-31 19:59:18 -0400

    Kth Smallest Element in Sorted Matrix

--- src/BinarySearch/KthSmallesNumberInMatrix/Main.java
+++ src/BinarySearch/KthSmallesNumberInMatrix/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         int[][] matrix = {{2, 7, 8, 9}, {5, 9, 10, 11}, {8, 9, 10, 15}, {9, 15, 20, 24}};
         System.out.println(new BruteForceSolution().KthSmallestElementInMatrix(matrix, 10));
+        System.out.println(new BinarySearch().KthSmallestElementInMatrix(matrix, 10));
     }
 
     public static class BruteForceSolution {
@@ -30,4 +31,29 @@ public class Main {
 
     }
 
+    public static class BinarySearch {
+
+        public int KthSmallestElementInMatrix(int[][] matrix, int k) {
+            int low = matrix[0][0];
+            int hi = matrix[matrix.length - 1][matrix[0].length - 1];
+            while (low < hi) {
+                int mid = low + (hi - low) / 2;
+                if (getSmallerOrEqualToTheNumber(matrix, mid) < k) low = mid + 1;
+                else hi = mid;
+            }
+            return low;
+        }
+
+        public int getSmallerOrEqualToTheNumber(int[][] matrix, int target) {
+            int counter = 0, i = matrix.length - 1, j = 0;
+            while (i >= 0 && j < matrix[0].length) {
+                if (matrix[i][j] > target) i--;
+                else {
+                    counter += i + 1;
+                    j++;
+                }
+            }
+            return counter;
+        }
+    }
 }
