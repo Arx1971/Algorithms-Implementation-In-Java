de8a8d1fa8c7deb44c881e18c75161ddcfc82336
Arx1971 <adnan.rahin13@gmail.com>
2020-09-18 18:43:01 -0400

    Trie Insertion Method Added

--- src/AdvancedDataStructure/Trie/Main.java
+++ src/AdvancedDataStructure/Trie/Main.java
@@ -1,8 +1,43 @@
 package AdvancedDataStructure.Trie;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+}
+
+class Trie {
+
+    public class TrieNode {
+        Map<Character, TrieNode> map;
+        boolean isWord;
+
+        public TrieNode() {
+            this.map = new HashMap<>();
+            this.isWord = false;
+        }
+    }
+
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode current = root;
+        for (int i = 0; i < word.length(); i++) {
+            TrieNode newNode = current.map.get(word.charAt(i));
+            if (newNode == null) {
+                newNode = new TrieNode();
+                current.map.put(word.charAt(i), newNode);
+            }
+            current = newNode;
+        }
+        current.isWord = true;
     }
 }
