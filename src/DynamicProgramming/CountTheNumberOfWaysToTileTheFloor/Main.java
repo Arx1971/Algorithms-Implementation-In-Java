0c38ec5456fa9f5281048154f7e11e3de7288df3
Arx1971 <adnan.rahin13@gmail.com>
2020-09-20 14:57:03 -0400

    Number of Ways to tile the Floor

--- src/DynamicProgramming/CountTheNumberOfWaysToTileTheFloor/Main.java
+++ src/DynamicProgramming/CountTheNumberOfWaysToTileTheFloor/Main.java
@@ -1,21 +1,30 @@
 package DynamicProgramming.CountTheNumberOfWaysToTileTheFloor;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(numberOfWaysTileNMFloor(5, 1));
+        System.out.println(numberOfWaysTileNMFloor(5, 2));
 
     }
 
     public static long numberOfWaysTileNMFloor(int n, int m) {
         if (n < m) return 1;
-        else if (n == m) return 2;
+        if (n == m) return 2;
 
-        double golderRatio = ((1 + Math.sqrt(5)) / 2);
-        double phi = Math.pow(golderRatio, n);
+        long mod = (long) (1e9 + 7);
 
-        return Math.round(((phi) / Math.sqrt(5)));
+        long[] dp = new long[n + 1];
+        Arrays.fill(dp, 1);
+        dp[m] = 2;
+        dp[0] = 0;
+
+        for (int i = m + 1; i <= n; i++) {
+            dp[i] = ((dp[i - 1] % mod) + (dp[i - m] % mod)) % mod;
+        }
+        return dp[n];
     }
 
 }
