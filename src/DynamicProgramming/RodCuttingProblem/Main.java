24f5e257039994ac55c305e341f0bff6d6de9e80
Arx1971 <adnan.rahin13@gmail.com>
2020-10-03 15:29:20 -0400

    Dp Problem

--- src/DynamicProgramming/RodCuttingProblem/Main.java
+++ src/DynamicProgramming/RodCuttingProblem/Main.java
@@ -3,9 +3,23 @@ package DynamicProgramming.RodCuttingProblem;
 public class Main {
 
     public static void main(String[] args) {
+        int[] arr = new int[]{1, 5, 8, 9, 10, 17, 17, 20};
 
+        System.out.println(rodCutting(arr));
     }
 
-    
+    public static int rodCutting(int[] prices) {
+        int n = prices.length;
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MIN_VALUE;
+            for (int j = 0; j < i; j++) {
+                dp[i] = Math.max(dp[i], prices[j] + dp[i - j - 1]);
+            }
+        }
+
+        return dp[n];
+    }
 
 }
