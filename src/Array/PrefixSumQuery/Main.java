6d6d642eb66506352bee46678b440afddab273fd
Arx1971 <adnan.rahin13@gmail.com>
2020-10-22 06:22:52 -0400

    RangeQuery Constructor Created

--- src/Array/PrefixSumQuery/Main.java
+++ src/Array/PrefixSumQuery/Main.java
@@ -4,25 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] prefixSum = getPrefixArray(new int[]{1, 2, 3, 4, 5});
-
-        System.out.println(getQuerySum(prefixSum, 0, 3));
-        System.out.println(getQuerySum(prefixSum, 0, 3));
+        
 
     }
 
-    public static int[] getPrefixArray(int[] arr) {
-        int[] prefixSum = new int[arr.length];
-        prefixSum[0] = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            prefixSum[i] = prefixSum[i - 1] + arr[i];
+    public static class RangeQuery {
+
+        private final int[] arr;
+
+        public RangeQuery(int[] arr) {
+            this.arr = arr;
         }
-        return prefixSum;
-    }
 
-    public static int getQuerySum(int[] prefixSum, int l, int r) {
-        if (l == 0) return prefixSum[r];
-        return prefixSum[r] - prefixSum[l - 1];
-    }
+        public int[] getPrefixArray() {
+            int[] prefixSum = new int[arr.length];
+            prefixSum[0] = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                prefixSum[i] = prefixSum[i - 1] + arr[i];
+            }
+            return prefixSum;
+        }
 
+        public int getQuerySum(int[] prefixSum, int l, int r) {
+            if (l == 0) return prefixSum[r];
+            return prefixSum[r] - prefixSum[l - 1];
+        }
+
+    }
 }
