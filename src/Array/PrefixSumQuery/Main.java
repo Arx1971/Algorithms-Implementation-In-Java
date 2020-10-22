c246d34f0ed985b9431b325df8df0e31015f1d50
Arx1971 <adnan.rahin13@gmail.com>
2020-10-22 08:25:56 -0400

    RangeQuery update

--- src/Array/PrefixSumQuery/Main.java
+++ src/Array/PrefixSumQuery/Main.java
@@ -4,19 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
+        System.out.println(new RangeQuery(new int[]{1, 2, 3, 4, 5}).getQuerySum(2, 3));
+        System.out.println(new RangeQuery(new int[]{1, 2, 3, 4, 5}).getQuerySum(0, 4));
+        
     }
 
     public static class RangeQuery {
 
-        private final int[] arr;
+        private final int[] prefixArray;
 
         public RangeQuery(int[] arr) {
-            this.arr = arr;
+            prefixArray = getPrefixArray(arr);
         }
 
-        public int[] getPrefixArray() {
+        private int[] getPrefixArray(int[] arr) {
             int[] prefixSum = new int[arr.length];
             prefixSum[0] = arr[0];
             for (int i = 1; i < arr.length; i++) {
@@ -25,9 +26,10 @@ public class Main {
             return prefixSum;
         }
 
-        public int getQuerySum(int[] prefixSum, int l, int r) {
-            if (l == 0) return prefixSum[r];
-            return prefixSum[r] - prefixSum[l - 1];
+        public int getQuerySum(int l, int r) {
+            if (prefixArray.length == 0) return 0;
+            if (l == 0) return prefixArray[r];
+            return prefixArray[r] - prefixArray[l - 1];
         }
 
     }
